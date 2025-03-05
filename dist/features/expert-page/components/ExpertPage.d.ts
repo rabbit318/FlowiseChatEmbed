7a19522f71989c435dd0f452487e7d8d36adc8f3
“rabbit318@yeah.net” <rabbit318@yeah.net>
2025-03-05 16:54:52 -0500

    rebuild

--- dist/features/expert-page/components/ExpertPage.d.ts
+++
@@ -1,7 +0,0 @@
-import { ExpertBotProps } from '@/components/ExpertBot';
-import { BubbleParams } from '@/features/bubble/types';
-export type ExpertPageProps = ExpertBotProps & BubbleParams;
-export declare const ExpertPage: (props: ExpertPageProps, { element }: {
-    element: HTMLElement;
-}) => import("solid-js").JSX.Element;
-//# sourceMappingURL=ExpertPage.d.ts.map
